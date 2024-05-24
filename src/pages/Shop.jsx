93762e56130ff8a2a453a5eedb672891a9020d51
ramohin1236 <108376758+ramohin1236@users.noreply.github.com>
2024-05-24 06:43:14 -0700

    pagination complete

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -4,23 +4,37 @@ import { useEffect, useState } from 'react'
 import ProductsCart from '../components/ProductsCart'
 import shirt from  '/public/shirt.png'
 import useAxiosPublic from '../hooks/useAxiosPublic'
+import { SiYr } from 'react-icons/si'
 const Shop = () => {
     const [product,setProduct]=useState(null)
     const axiosPublic = useAxiosPublic()
 
+   
+
+    const [filteredItems, setFilteredItems]=useState([])
+    const [currentPage,setCurrentPage]=useState(1)
+    const [itemsPerPage]= useState(7)
+
+    const indexOfLastItem= currentPage*itemsPerPage;
+    const indexOfFirstItem= indexOfLastItem- itemsPerPage;
+    const currentItems = filteredItems.slice(indexOfFirstItem,indexOfLastItem);
+    const paginate = (pageNumber)=>setCurrentPage(pageNumber)
+
+
+
     useEffect(()=>{
-        axiosPublic.get('/product')
-        .then(data=>setProduct(data.data))
+        try{
+            axiosPublic.get('/product')
+            .then(data=>{
+                setProduct(data.data)
+                setFilteredItems(data.data)
+            })
+        }catch(err){
+            console.log("error for fetching data",err);  
+        }
+        
     },[axiosPublic])
 
-    // const [filteredItems, setFilteredItems]=useState([])
-    // const [currentPage,setCurrentPage]=useState(1)
-    // const [itemsPerPage]= useState(10)
-
-    // const indexOfLastItem= currentPage*itemsPerPage;
-    // const indexOfFirstItem= indexOfLastItem- itemsPerPage;
-    // const currentItems = filteredItems.slice(indexOfFirstItem,indexOfLastItem);
-    // const paginate = (pageNumber)=>setCurrentPage(pageNumber)
     return (
         <div>
             {/* shop banner start */}
@@ -56,25 +70,25 @@ const Shop = () => {
               <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 container mx-auto'> 
                    {/* products starts */}
                    {
-                    product?.map((item,idx)=> <ProductsCart key={idx} item={item}/>)
+                    currentItems?.map((item,idx)=> <ProductsCart key={idx} item={item}/>)
                    }
                     
                     
                    {/* products ends */}
               </div>
-                {/* pagination */}
-         {/* <div className='flex justify-center mt-6 gap-3'>
+               {/* pagination */}
+         <div className='flex justify-center mt-6 gap-3'>
               {
               Array.from({length: Math.ceil(filteredItems.length / itemsPerPage)}).map((_,idx)=>(
                 <button
-                className={`mx-1 px-3 py-1 rounded-full ${currentPage === idx+1 ? "bg-button text-white text-2xl font-bold":"font-bold text-2xl bg-gray-300"}`}
+                className={`mx-1 px-3 py-1 rounded-full ${currentPage === idx+1 ? "bg-button text-black text-2xl font-bold":"font-bold text-2xl bg-gray-300"}`}
                 onClick={()=>paginate(idx+1)}
                 key={idx+1}>
                     {idx+1}
                 </button>
               ))
               }
-         </div> */}
+         </div>
               </div>
         </div>
     )
